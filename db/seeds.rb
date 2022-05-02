28a870a29c8d8088399afdefbf1d843b651e702d
MirsonCamadro <kztempus@gmail.com>
2022-05-02 11:20:16 -0400

    added index and gem faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do |i|
+    Post.create(
+        title: Faker::Movies::Lebowski.actor,
+        description: Faker::Movies::Lebowski.quote,
+        image_url: Faker::Avatar.image(size: "100x100")
+    )
+end
